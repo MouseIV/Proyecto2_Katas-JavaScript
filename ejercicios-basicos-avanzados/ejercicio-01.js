b0e4aed08c71b77061df10beeaf6cdc6d98955cb
Diego S. <guipas.diego@gmail.com>
2026-05-09 12:37:16 +0200

    Update ejercicio-01.js

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,19 @@
+// 1. Superhéroe favorito
+let miSuperheroeFavorito = "Hulk";
+
+// 2. Número favorito
+let numeroFavorito = 50;
+
+// 3. Dimensiones
+let altura = 5;
+let longitud = 10;
+
+// 4. Suma
+let suma = altura + longitud;
+
+// 5. Mostrar todo por consola
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
